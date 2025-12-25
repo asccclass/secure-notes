47fdff0d7ab45e7d0b55253e4c5c0459af815552
Andy Liu <justgps@gmail.com>
2025-12-25 10:24:25 +0800

    <docs>修改為中文

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// 简单的 Service Worker，满足 PWA 安装要求
+// 簡單的 Service Worker，滿足 PWA 安裝要求
 self.addEventListener('install', (e) => {
     console.log('[Service Worker] Install');
     e.waitUntil(self.skipWaiting());
@@ -10,6 +10,6 @@ self.addEventListener('activate', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-    // 简单的透传策略，确保有 fetch handler
+    // 簡單的透傳策略，確保有 fetch handler
     e.respondWith(fetch(e.request));
 });
